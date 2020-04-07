bec0fae5c40810a9ca018cecc9d0ef46a9f71d5d
Christian Grobmeier <cg@grobmeier.de>
2020-04-07 13:28:44 +0200

    added empty controller

---
+++ src/main/java/com/accenture/js/intellicats/shop/controllers/ProductController.java
@@ -0,0 +1,10 @@
+package com.accenture.js.intellicats.shop.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    
+
+}
