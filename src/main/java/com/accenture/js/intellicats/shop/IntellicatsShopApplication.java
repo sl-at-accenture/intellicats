07a7c86fa7e2dc0424fed6684ea2ec617b561ef6
Christian Grobmeier <cg@grobmeier.de>
2020-04-07 09:27:29 +0200

    added spring archetype

---
+++ src/main/java/com/accenture/js/intellicats/shop/IntellicatsShopApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.js.intellicats.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellicatsShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellicatsShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/js/intellicats/shop/IntellicatsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.js.intellicats.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellicatsShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
